b6cac9799a148c012ea65238f3e46de0ae5a15ea
Marsel <47145234+chelovek-gorod@users.noreply.github.com>
2025-02-10 16:39:29 +0300

    Update main.js

--- dev_js/engine/main.js
+++ dev_js/engine/main.js
@@ -2,6 +2,10 @@ import { Assets } from 'pixi.js'
 import { fonts, uploadAssets } from './loader'
 import { initFontStyles } from './fonts'
 import { startGame } from '../game'
+import { EventHub, events } from './engine/events'
+
+let level = null
+EventHub.on( events.setLevel, (data) => level = data )
 
 // preload fonts
 Assets.addBundle('fonts', fonts)
@@ -9,5 +13,26 @@ Assets.loadBundle('fonts').then( fontsData => {
     // update font values by font family
     for(let key in fontsData) fonts[key] = fontsData[key].family
     initFontStyles()
-    uploadAssets( startGame )
+    uploadAssets( awaitLevel )
 })
+
+function awaitLevel() {
+    if (level) startGame(level)
+    setTimeout(awaitLevel, 100)
+    console.log("awaitLevel")
+}
+
+/*
+
+const level = [
+    [0, 0, 1, 1, 1, 0, 0, 1, 0],
+    [1, 1, 1, 0, 1, 0, 0, 1, 0],
+    [1, 0, 0, 0, 1, 0, 0, 1, 0],
+    [1, 0, 1, 1, 1, 0, 0, 0, 0],
+    [3, 0, 1, 1, 0, 0, 1, 1, 1],
+    [0, 0, 1, 1, 0, 0, 1, 2, 1],
+    [0, 0, 0, 1, 1, 1, 1, 1, 1],
+]
+
+
+*/
